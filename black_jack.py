0909bd264e8695f35f5d0189424aa8894261a424
Monty <tuckerjsarah@gmail.com>
2019-11-24 12:43:06 -0500

    dsiplay editing

--- black_jack.py
+++ black_jack.py
@@ -1,14 +1,49 @@
 # display black jack table showing one card up from dealer and one card down from dealer then two cards up for player
 def display_initial(dealer,player):
     print('Dealer:')
-    print('---------        ---------')
-    print('|        |        |        |')
-    print(f'|     {dealer.cards[0].face}     |        |        |')
-    print('|   of   |        |        |')
-    print(f'|     {dealer.cards[0].suit}     |        |        |')
-    print('|         |        |        |')
-    print('---------        ---------')
-    #insert loop through player cards and display all face up
+    print('------------        ------------')
+    print('|          |        |          |')
+    if type(dealer.cards[0].face) != int:
+        total_len = 8 - len(dealer.cards[0].face)
+    elif dealer.cards[0].face == 10:
+        total_len = 6
+    else:
+        total_len = 7
+    face_print = f'|  {dealer.cards[0].face}'
+    for _ in range(0,total_len):
+        face_print += ' '
+    print(f'{face_print}|        |          |')
+    print('|  of      |        |          |')
+    total_len = 8 - len(dealer.cards[0].suit)
+    suit_print = f'|  {dealer.cards[0].suit}'
+    for _ in range(0,total_len):
+        suit_print += ' '
+    print(f'{suit_print}|        |          |')
+    print('|          |        |          |')
+    print('------------        ------------')
+    
+    print('Player:')
+    for i in player.cards:
+        print('------------')
+        print('|          |')
+        if type(i.face) != int:
+            total_len = 8 - len(i.face)
+        elif i.face == 10:
+            total_len = 6
+        else:
+            total_len = 7
+        face_print = f'|  {i.face}'
+        for _ in range(0,total_len):
+            face_print += ' '
+        print(f'{face_print}|')
+        print('|  of      |')
+        total_len = 8 - len(i.suit)
+        suit_print = f'|  {i.suit}'
+        for _ in range(0,total_len):
+            suit_print += ' '
+        print(f'{suit_print}|')
+        print('|          |')
+        print('------------')
 
 # create a second display function for when its the dealers turn
 
@@ -31,16 +66,9 @@ def create_deck():
 
 #create a dealer object- will need to know which cards they have and their total count
 class Dealer:
-    def __init__(self, cards, total):
+    def __init__(self, cards):
         self.cards = cards
-        self.total = total
 
-#create a player object- will need to know the cards they have, the total count for the cards, the amount of money to bet and if you have time a win streak tracker (if they win 3 in a row)
-class Player:
-    def __init__(self, cards, account, wins):
-        self.cards = cards
-        self.account = account
-        self.wins = wins
     def total(self):
         total = 0
         for card in self.cards:
@@ -55,14 +83,34 @@ class Player:
             else:
                 continue
         return total
-    def bet(self, amount):
-        return amount <= self.account
+
+#create a player object- will need to know the cards they have, the total count for the cards, the amount of money to bet and if you have time a win streak tracker (if they win 3 in a row)
+class Player:
+    def __init__(self, cards, account, wins):
+        self.cards = cards
+        self.account = account
+        self.wins = wins
+
+    def total(self):
+        total = 0
+        for card in self.cards:
+            if card.face == 'King':
+                total += 13
+            elif card.face == 'Queen':
+                total += 12
+            elif card.face == 'Jack':
+                total += 11
+            elif card.face in [2,3,4,5,6,7,8,9,10]:
+                total += card.face
+            else:
+                continue
+        return total
 
 #function to ask the player what they would like to bet. check to ensure this does not exceed the total money in the player's account
 def bet(player):
     amount = int(input('How much would you like to bet? '))
     while amount > player.account:
-        input(f'Sorry, this amount exceeds your funds of {player.account}. What would you like to bet? ')
+        amount = int(input(f'Sorry, this amount exceeds your funds of {player.account}. What would you like to bet? '))
     return amount 
 
 #function to ask the player if they would like to stand or hit
@@ -70,10 +118,13 @@ def stand_hit():
     return input('Would you like to stand or hit? (s/h)')
 
 #function to randomly draw a card from the deck
-def draw(deck):
+def draw(deck, existing):
     import random
     index = random.randint(0,51)
-    return deck[index]
+    while deck[index] in existing:
+        index = random.randint(0,51)
+    existing.append(deck[index])
+    return deck[index], existing
 
 #function to check that the randomly drawn card has not already been pulled- (check against dealer.cards and player.cards). if so re-draw
 
@@ -91,19 +142,6 @@ def dealer_turn(player, dealer, deck):
         else:
             return dealer.total
 
-#function to hit. if the player chooses to hit this function will determine the count of the card to be added to the player object
-def hit(card):
-    if card.face == 'King':
-        value = 13
-    elif card.face == 'Queen':
-        value = 12
-    elif card.face == 'Jack':
-        value = 11
-    elif card.face in [2,3,4,5,6,7,8,9,10]:
-        value = int(card.face)
-    else:
-        value = [1,11]
-    return value
 
 #function to determine which Ace value to use
 def ace(player):
@@ -154,4 +192,16 @@ card2 = Card('Hearts', 5)
 
 Sarah = Player([card1, card2], 100, 0)
 
-print(Sarah.total())
+def play():
+    deck = create_deck()
+    existing_cards = []
+    dealer_card1, existing_cards = draw(deck, existing_cards)
+    dealer_card2, existing_cards = draw(deck, existing_cards)
+    player_card1, existing_cards = draw(deck, existing_cards)
+    player_card2, existing_cards = draw(deck, existing_cards)
+    dealer = Dealer([dealer_card1,dealer_card2])
+    player = Player([player_card1, player_card2], 100, 0)
+    display_initial(dealer, player)
+
+
+play()
